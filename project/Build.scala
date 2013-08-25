75e2f012bdc6c1a8a4b7108df1d085a360ed18c2
retroryan <ryan@grandcloud.com>
2013-08-25 19:11:31 -0300

    fixing build version

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "complexBanking"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "1.0-a"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
